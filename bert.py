17f88ea8d6c915070053a24c6de532f46418cdf7
Zichu <527200054@qq.com>
2021-11-14 13:05:39 -0500

    bert created

---
+++ bert.py
@@ -0,0 +1,237 @@
+#%%
+#https://albertauyeung.github.io/2020/06/19/bert-tokenization.html/
+from transformers import BertTokenizer
+tz = BertTokenizer.from_pretrained("bert-base-cased")
+tz.convert_tokens_to_ids(["characteristically"])
+[100]
+
+sent = "He remains characteristically confident and optimistic."
+tokens=tz.tokenize(sent)
+
+
+tz.convert_tokens_to_ids(tz.tokenize(sent))
+
+# %%
+#train with BERT, XLnet and Roberta for classification
+#https://towardsdatascience.com/transformers-for-multilabel-classification-71a1a0daf5e1
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import torch
+from torch.nn import BCEWithLogitsLoss, BCELoss
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics import classification_report, confusion_matrix, multilabel_confusion_matrix, f1_score, accuracy_score
+import pickle
+from transformers import *
+from tqdm import tqdm, trange
+from ast import literal_eval
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#%%
+df = pd.read_csv('/Users/chenzichu/Desktop/Capstone/data/df_processed.csv') #jigsaw-toxic-comment-classification-challenge
+
+#%%
+df_10 = df.loc[df['length'] >= 10].reset_index() 
+df_20 = df.loc[df['length'] >= 20].reset_index()
+df_30 = df.loc[df['length'] >= 30].reset_index()
+
+df=df_20
+#%%
+col_names=['Action', 'Adventure',
+       'Animation &amp; Modeling', 'Audio Production', 'Casual',
+       'Design &amp; Illustration', 'Early Access', 'Education',
+       'Free to Play', 'Indie', 'Massively Multiplayer', 'Photo Editing',
+       'RPG', 'Racing', 'Simulation', 'Software Training', 'Sports',
+       'Strategy', 'Utilities', 'Video Production', 'Web Publishing']
+num_labels=len(col_names)
+labels = list(df[col_names].values)
+comments = list(df["text"].values)
+# %%
+
+max_length = 100
+#Bert
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True) # tokenizer
+
+# XLNet:
+# tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased', do_lower_case=False) 
+# RoBERTa:
+# tokenizer = RobertaTokenizer.from_pretrained('roberta-base', do_lower_case=False)
+
+encodings = tokenizer.batch_encode_plus(comments,max_length=max_length,padding=True,truncation=True) # tokenizer's encoding method
+print('tokenizer outputs: ', encodings.keys())
+
+#%%
+
+input_ids = encodings['input_ids'] # tokenized and encoded sentences
+token_type_ids = encodings['token_type_ids'] # token type ids
+attention_masks = encodings['attention_mask'] # attention masks
+
+#%%
+# Identifying indices of 'one_hot_labels' entries that only occur once - this will allow us to stratify split our training data later
+label_counts = df.one_hot_labels.astype(str).value_counts()
+one_freq = label_counts[label_counts==1].keys()
+one_freq_idxs = sorted(list(df[df.one_hot_labels.astype(str).isin(one_freq)].index), reverse=True)
+
+
+# Gathering single instance inputs to force into the training set after stratified split
+one_freq_input_ids = [input_ids.pop(i) for i in one_freq_idxs]
+one_freq_token_types = [token_type_ids.pop(i) for i in one_freq_idxs]
+one_freq_attention_masks = [attention_masks.pop(i) for i in one_freq_idxs]
+one_freq_labels = [labels.pop(i) for i in one_freq_idxs]
+
+
+# Use train_test_split to split our data into train and validation sets
+
+train_inputs, validation_inputs, train_labels, validation_labels, train_token_types, validation_token_types, train_masks, validation_masks = train_test_split(input_ids, labels, token_type_ids,attention_masks,
+                                                            random_state=2021, test_size=0.2, stratify = labels)
+
+
+# Add one frequency data to train data
+train_inputs.extend(one_freq_input_ids)
+train_labels.extend(one_freq_labels)
+train_masks.extend(one_freq_attention_masks)
+train_token_types.extend(one_freq_token_types)
+
+# Convert all of our data into torch tensors, the required datatype for our model
+train_inputs = torch.tensor(train_inputs)
+train_labels = torch.tensor(train_labels)
+train_masks = torch.tensor(train_masks)
+train_token_types = torch.tensor(train_token_types)
+
+validation_inputs = torch.tensor(validation_inputs)
+validation_labels = torch.tensor(validation_labels)
+validation_masks = torch.tensor(validation_masks)
+validation_token_types = torch.tensor(validation_token_types)
+
+#%%
+# Select a batch size for training. 16/32/64/128
+batch_size = 32
+
+# Dataloader
+train_data = TensorDataset(train_inputs, train_masks, train_labels, train_token_types)
+train_sampler = RandomSampler(train_data)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+
+validation_data = TensorDataset(validation_inputs, validation_masks, validation_labels, validation_token_types)
+validation_sampler = SequentialSampler(validation_data)
+validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=batch_size)
+
+#%%
+model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=num_labels)
+model.cuda()
+
+# setting custom optimization parameters. You may implement a scheduler here as well.
+param_optimizer = list(model.named_parameters())
+no_decay = ['bias', 'gamma', 'beta']
+optimizer_grouped_parameters = [
+    {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+     'weight_decay_rate': 0.01},
+    {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+     'weight_decay_rate': 0.0}
+]
+
+optimizer = AdamW(optimizer_grouped_parameters,lr=2e-5,correct_bias=True)
+
+#%%
+# Store our loss and accuracy for plotting
+train_loss_set = []
+
+# Number of training epochs (authors recommend between 2 and 4)
+epochs = 5
+
+# trange is a tqdm wrapper around the normal python range
+for _ in trange(epochs, desc="Epoch"):
+
+  # Training
+  
+  # Set our model to training mode (as opposed to evaluation mode)
+  model.train()
+
+  # Tracking variables
+  tr_loss = 0 #running loss
+  nb_tr_examples, nb_tr_steps = 0, 0
+  
+  # Train the data for one epoch
+  for step, batch in enumerate(train_dataloader):
+    # Add batch to GPU
+    batch = tuple(t.to(device) for t in batch)
+    # Unpack the inputs from our dataloader
+    b_input_ids, b_input_mask, b_labels, b_token_types = batch
+    # Clear out the gradients (by default they accumulate)
+    optimizer.zero_grad()
+
+    # # Forward pass for multiclass classification
+    # outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask, labels=b_labels)
+    # loss = outputs[0]
+    # logits = outputs[1]
+
+    # Forward pass for multilabel classification
+    outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+    logits = outputs[0]
+    loss_func = BCEWithLogitsLoss() 
+    loss = loss_func(logits.view(-1,num_labels),b_labels.type_as(logits).view(-1,num_labels)) #convert labels to float for calculation
+    # loss_func = BCELoss() 
+    # loss = loss_func(torch.sigmoid(logits.view(-1,num_labels)),b_labels.type_as(logits).view(-1,num_labels)) #convert labels to float for calculation
+    train_loss_set.append(loss.item())    
+
+    # Backward pass
+    loss.backward()
+    # Update parameters and take a step using the computed gradient
+    optimizer.step()
+    # scheduler.step()
+    # Update tracking variables
+    tr_loss += loss.item()
+    nb_tr_examples += b_input_ids.size(0)
+    nb_tr_steps += 1
+
+  print("Train loss: {}".format(tr_loss/nb_tr_steps))
+
+###############################################################################
+
+  # Validation
+
+  # Put model in evaluation mode to evaluate loss on the validation set
+  model.eval()
+
+  # Variables to gather full output
+  logit_preds,true_labels,pred_labels,tokenized_texts = [],[],[],[]
+
+  # Predict
+  for i, batch in enumerate(validation_dataloader):
+    batch = tuple(t.to(device) for t in batch)
+    # Unpack the inputs from our dataloader
+    b_input_ids, b_input_mask, b_labels, b_token_types = batch
+    with torch.no_grad():
+      # Forward pass
+      outs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+      b_logit_pred = outs[0]
+      pred_label = torch.sigmoid(b_logit_pred)
+
+      b_logit_pred = b_logit_pred.detach().cpu().numpy()
+      pred_label = pred_label.to('cpu').numpy()
+      b_labels = b_labels.to('cpu').numpy()
+
+    tokenized_texts.append(b_input_ids)
+    logit_preds.append(b_logit_pred)
+    true_labels.append(b_labels)
+    pred_labels.append(pred_label)
+
+  # Flatten outputs
+  pred_labels = [item for sublist in pred_labels for item in sublist]
+  true_labels = [item for sublist in true_labels for item in sublist]
+
+  # Calculate Accuracy
+  threshold = 0.50
+  pred_bools = [pl>threshold for pl in pred_labels]
+  true_bools = [tl==1 for tl in true_labels]
+  val_f1_accuracy = f1_score(true_bools,pred_bools,average='micro')*100
+  val_flat_accuracy = accuracy_score(true_bools, pred_bools)*100
+
+  print('F1 Validation Accuracy: ', val_f1_accuracy)
+  print('Flat Validation Accuracy: ', val_flat_accuracy)
+
+
+#%%
+#Prediction
